a90dbccbee94ba6540cbfe7bd27f4cd5e4266313
Mark Atherton <mark@insightinternet.co.uk>
2023-05-14 16:01:36 +0100

    displayed users from server on React frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,15 @@ function App() {
       });
   }, []);
 
-  return <div>Hello World</div>;
+  return (
+    <div>
+      {typeof backendData.users === "undefined" ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, index) => <p key={index}>{user}</p>)
+      )}
+    </div>
+  );
 }
 
 export default App;
