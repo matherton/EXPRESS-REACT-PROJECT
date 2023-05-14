304fc5865d722aca6c2a8906906cab7b90c95a88
Mark Atherton <mark@insightinternet.co.uk>
2023-05-14 15:45:06 +0100

    setup useState and useEffect to handle the fetch /api response

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
+
   return <div>Hello World</div>;
 }
 
