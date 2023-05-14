b6c35c370e7eb94c9d19f543a3d35332799f5207
Mark Atherton <mark@insightinternet.co.uk>
2023-05-14 16:05:58 +0100

    added users in server to make sure they display correctly in React frontend

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.json({ users: ["user1", "user2", "user3"] });
+  res.json({ users: ["user1", "user2", "user3", "user4", "user5"] });
 });
 
 app.listen(5000, () => {
